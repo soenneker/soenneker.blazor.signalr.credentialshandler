2f029d280377e6eb1367d9f563c003fdcb4fb5da
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:20 -0500

    TUnit conversion

--- test/Soenneker.Blazor.SignalR.CredentialsHandler.Tests/BlazorSignalRCredentialsHandlerTests.cs
+++ test/Soenneker.Blazor.SignalR.CredentialsHandler.Tests/BlazorSignalRCredentialsHandlerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Blazor.SignalR.CredentialsHandler.Tests;
 
-[Collection("Collection")]
-public class BlazorSignalRCredentialsHandlerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class BlazorSignalRCredentialsHandlerTests : HostedUnitTest
 {
-    public BlazorSignalRCredentialsHandlerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public BlazorSignalRCredentialsHandlerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
